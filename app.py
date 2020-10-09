a2f3aa12e2adcbe0dc1de9a2cfc8e3ab5fb82c51
Nithin <sainithin0821@gmail.com>
2020-10-09 21:04:30 +0530

    New testings

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
+print('Starting to import the packages')
 import flask
 from flask import request
 
+from dependencies import Dependenciesnow
+print('Imported the packages and now creating the flask app.')
 app = flask.Flask(__name__)
-
+var = Dependenciesnow()
+print("Flask app has been created.")
 @app.route('/',methods=['GET'])
 def hello_world():
-    #print("Inside Hello World!")
-    if request.environ['SERVER_PROTOCOL'] != "HTTP/1.1":
-        flask.abort(406)
-        return "Access Denied"
-    else:
-        return "Hello Word"
-    print(request.environ['SERVER_PROTOCOL'])
-    #return "Hello"
-
-app.run('0.0.0.0',port = 5555,debug=True)
+    print("Inside hello world")
+    return "Hi"
+print("App will now run.")
+app.run('0.0.0.0',port = 1234,debug=True)
---
+++ dependencies.py
@@ -0,0 +1,12 @@
+
+
+class Dependenciesnow():
+    print('Inside the Dependency class.')
+    def __init__(self):
+        print("Inside the constructor.")
+
+    def sample_get(self):
+        print("Inside sample get method.")
+        return 'Hello'
+    
+    print('End of the Dependencies class.')
